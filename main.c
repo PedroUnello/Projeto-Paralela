4f136c0fae97e16c8d4c7d4448aa1f9bd08999a7
An-225 <66622436+An-225@users.noreply.github.com>
2021-10-30 16:54:33 -0300

    rascunho tartaruga

--- main.c
+++ main.c
@@ -1,29 +1,51 @@
 #include <stdio.h>
-#include <time.h>    
+#include <time.h>
+#include <omp.h>
 
- 
-#define BILLION  1000000000.0
- 
+#define BILLION 1000000000.0
+
+void tartaruga(int inicio, int fim, double *soma);
 
 int main()
 {
-    // Inicio do clock
-    struct timespec start, end;
-    clock_gettime(CLOCK_REALTIME, &start);
+  // Inicio do clock
+  struct timespec start, end;
+  clock_gettime(CLOCK_REALTIME, &start);
 
+  // start code
+  printf("teste de codigo");
+  for (int i = 0; i < 10000; i++)
+  {
+    printf("");
+  }
+  // end code
+
+  //Fim do clock
+  clock_gettime(CLOCK_REALTIME, &end);
+  double time_spent = (end.tv_sec - start.tv_sec) +
+                      (end.tv_nsec - start.tv_nsec) / BILLION;
+  printf("\n\nTempo de execução %f ", time_spent);
+  return 0;
+}
+
+void tartaruga(int inicio, int fim, double *soma)
+{
+  double somaLocal;
+  int contador = inicio;
+
+  int idThread = omp_get_thread_num();
+  int qtdThreads = omp_get_num_threads();
+
+  while (contador <= fim)
+  {
+    if (contador%qtdThreads == idThread)
+    {
+      somaLocal = 1/contador;
+    }
+    
+    contador++;
+  }
   
-    // start code
-      printf("teste de codigo");
-      for(int i = 0; i < 10000; i++){
-        printf("");
-      }
-    // end code
-
-
-    //Fim do clock
-    clock_gettime(CLOCK_REALTIME, &end);
-    double time_spent = (end.tv_sec - start.tv_sec) +
-                        (end.tv_nsec - start.tv_nsec) / BILLION;
-    printf("\n\nTempo de execução %f ", time_spent);
-    return 0;
+#pragma omp critical
+  *soma += somaLocal;
 }

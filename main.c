e92378f66a5d0f355e276536b352cf838d926edd
An-225 <66622436+An-225@users.noreply.github.com>
2021-10-30 17:37:07 -0300

    Funcional, mas não rápida

--- main.c
+++ main.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
-#include <time.h>    
+#include <stdlib.h>
+#include <time.h>
 #include <omp.h>
 
-#define BILLION  1000000000.0
+#define BILLION 1000000000.0
 
 void tartaruga(int inicio, int fim, double *soma);
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   // Inicio do clock
   struct timespec start, end;
   clock_gettime(CLOCK_REALTIME, &start);
 
+  // *****************************************************
+  //                    INICIALIZAÇÃO
+  // *****************************************************
+  double soma = 0;
+  int inicio = 1, fim = 1000000000;
+  int thread_count;
 
-  
-    // *****************************************************
-    //                    INICIALIZAÇÃO
-    // *****************************************************
-    double soma;
-    int inicio, fim;
-    int thread_count;
+  thread_count = strtol(argv[1], NULL, 10);
+#pragma omp parallel num_threads(thread_count)
+  tartaruga(inicio, fim, &soma);
 
-    thread_count = strtol(argv[1], NULL, 10);
-    # pragma omp parallel num_threads(thread_count)
-    tartaruga(inicio,fim,&soma);
+  // ******************************************************
 
-    // ******************************************************
-    
+  //Fim do clock
+  clock_gettime(CLOCK_REALTIME, &end);
+  double time_spent = (end.tv_sec - start.tv_sec) +
+                      (end.tv_nsec - start.tv_nsec) / BILLION;
+  printf("Tempo de execução %f ", time_spent);
 
-    //Fim do clock
-    clock_gettime(CLOCK_REALTIME, &end);
-    double time_spent = (end.tv_sec - start.tv_sec) +
-                        (end.tv_nsec - start.tv_nsec) / BILLION;
-    printf("\n\nTempo de execução %f ", time_spent);
-    return 0;
+  printf("Soma = %lf\n", soma);
+  return 0;
 }
 
 void tartaruga(int inicio, int fim, double *soma)
 {
-  double somaLocal;
+  double resultado;
   int contador = inicio;
 
   int idThread = omp_get_thread_num();
@@ -46,14 +46,19 @@ void tartaruga(int inicio, int fim, double *soma)
 
   while (contador <= fim)
   {
-    if (contador%qtdThreads == idThread)
+    if (contador % qtdThreads == idThread)
     {
-      somaLocal = 1/contador;
+      resultado = 1 / (double)contador;
+
+      /*
+      printf("ID: %d | QTD: %d\n",idThread,qtdThreads);
+      printf("R: %.14e | C: %d\n",resultado,contador);
+      */
+
+#pragma omp critical
+      *soma += resultado;
     }
-    
+
     contador++;
   }
-  
-#pragma omp critical
-  *soma += somaLocal;
 }
